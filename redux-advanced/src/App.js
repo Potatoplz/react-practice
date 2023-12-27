01bd450867cbee72d8a05cc69b5640decd18b0c1
Potatoplz <beneh0710@gmail.com>
2023-12-27 15:59:51 +0900

    redux로 http state 및 피드백 처리하기

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -1,17 +1,85 @@
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiActions } from './store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+
+let isInitial = true; // 컴포넌트 함수 외부에서 정의하면 컴포넌트가 재렌더링되도 초기화되지 않고, 이 파일이 처음으로 parse될 때 초기화 된다.
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const response = await fetch('http://localhost:8080/cart', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      //const responseData = await response.json();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      // sendCartData함수 내부에서 발생할 수 있는 오류 캐치한 다음,
+      // 오류가 있는 showNotification작업을 디스패치
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [cart, dispatch]); // cart가 변경될 때마다 useEffect를 실행.
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
