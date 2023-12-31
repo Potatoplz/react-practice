2caa200759d82d94fa5094f0354206ce4055aeb5
Potatoplz <beneh0710@gmail.com>
2023-12-31 18:34:07 +0900

    인증 action 실행하기

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,38 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+// action(): AuthForm이 전송될 때마다 트리거 되는 함수
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login'; // default: login
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+  }
+
+  const data = await request.formData(); //입력값을 가져온다.
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
+
+  // soon: manage that token
+  return redirect('/'); // 메인으로 리턴
+}
